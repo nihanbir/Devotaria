280f70422d312876b6b856440f22f3941f1661dc
nihanbir <nihan.bir@gmail.com>
2025-10-08 16:26:37 +0200

    create helperUtilities

---
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Utilities
+{
+    public static class HelperUtilities
+    {
+        public static bool ValidateCheckEmptyString(Object thisObject, string fieldName, string stringToCheck)
+        {
+            if (stringToCheck != "") return false;
+            Debug.Log(fieldName + " is empty and must contain a value in object " + thisObject.name);
+            return true;
+        }
+        
+        /// <summary>
+        /// list empty or contains null value check - returns true if there is an error
+        /// </summary>
+        public static bool ValidateCheckEnumerableValues(
+            Object thisObject,
+            string fieldName,
+            IEnumerable enumerableObjectToCheck)
+        {
+            bool error = false;
+            int count = 0;
+            foreach (var item in enumerableObjectToCheck)
+            {
+                if (item == null)
+                {
+                    Debug.Log(fieldName + " has null values in object " + thisObject.name);
+                    error = true;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                Debug.Log(fieldName + " has no values in object " + thisObject.name);
+                error = true;
+            }
+            return error;
+        }
+    }
+}
