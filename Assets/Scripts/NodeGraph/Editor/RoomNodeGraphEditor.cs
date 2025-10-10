e98799aa327aba37612674eedf4dfc0f5b8f89ed
nihanbir <nihan.bir@gmail.com>
2025-10-10 14:46:34 +0200

    restructure the script

--- Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -7,6 +7,8 @@ namespace NodeGraph.Editor
 {
     public class RoomNodeGraphEditor : EditorWindow
     {
+        #region Variables
+        
         private GUIStyle _roomNodeStyle;
         private GUIStyle _roomNodeSelectedStyle;
         private static RoomNodeGraphSO _currentRoomNodeGraph;
@@ -23,12 +25,28 @@ namespace NodeGraph.Editor
         private const float ConnectingLineWidth = 3f;
         private const float ConnectingLineArrowSize = 6f;
         
+        #endregion Variables
+        
+        #region Graph Editor SO Asset
         //Define where the editor window will be created in the Unity menu
         [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
-        
         public static void OpenWindow()
         {
             GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+        }
+        
+        /// <summary>
+        /// Open the room node graph editor window if a room node graph SO asset is double-clicked in the inspector
+        /// </summary>
+        [OnOpenAsset(0)] //First callback to be executed when the asset is double-clicked
+        public static bool OnDoubleClickAsset(int instanceID, int line)
+        {
+            RoomNodeGraphSO roomNodeGraph = EditorUtility.InstanceIDToObject(instanceID) as RoomNodeGraphSO;
+            if (roomNodeGraph == null) return false;
+            
+            OpenWindow();
+            _currentRoomNodeGraph = roomNodeGraph;
+            return true;
         }
         
         private void OnEnable()
@@ -79,25 +97,11 @@ namespace NodeGraph.Editor
         }
 
         /// <summary>
-        /// Open the room node graph editor window if a room node graph scriptable object asset is double-clicked in the inspector
-        /// </summary>
-        [OnOpenAsset(0)] //First callback to be executed when the asset is double-clicked
-        public static bool OnDoubleClickAsset(int instanceID, int line)
-        {
-            RoomNodeGraphSO roomNodeGraph = EditorUtility.InstanceIDToObject(instanceID) as RoomNodeGraphSO;
-            if (roomNodeGraph == null) return false;
-            
-            OpenWindow();
-            _currentRoomNodeGraph = roomNodeGraph;
-            return true;
-        }
-        
-        /// <summary>
         /// Draw editor GUI
         /// </summary>
         private void OnGUI()
         {
-            //If a scriptable object of type RoomNodeGraphSO has been selected then process
+            //If a SO of type RoomNodeGraphSO has been selected then process
             if (_currentRoomNodeGraph)
             {
                 DrawDraggedLine();
@@ -114,7 +118,188 @@ namespace NodeGraph.Editor
                 Repaint();
             }
         }
+        
+        #endregion Graph Editor SO Asset
+        
+        #region Event Processors
+        private void ProcessEvents(Event currentEvent)
+        {
+            // If the node is null
+            // or if the mouse is not currently dragging the node 
+            // then set the current room node
+            if (!_currentRoomNode || _currentRoomNode.isLeftClickDragging == false)
+            {
+                _currentRoomNode = GetHoveredRoomNode(currentEvent);
+            }
+            
+            // If the current room node is null,
+            // or we are currently dragging a line from the room node
+            // then process the room node graph events
+            if (!_currentRoomNode || _currentRoomNodeGraph.roomNodeToDrawLineFrom)
+            {
+                ProcessRoomNodeGraphEvents(currentEvent);
+            }
+            // Else process the room node events
+            else
+            {
+                _currentRoomNode.ProcessEvents(currentEvent);
+            }
+        }
 
+        private void ProcessRoomNodeGraphEvents(Event currentEvent)
+        {
+            switch (currentEvent.type)
+            {
+                case EventType.MouseDown:
+                    ProcessMouseDownEvent(currentEvent);
+                    break;
+                
+                case EventType.MouseUp:
+                    ProcessMouseUpEvent(currentEvent);
+                    break;
+                
+                case EventType.MouseDrag:
+                    ProcessMouseDragEvent(currentEvent);
+                    break;
+                
+            }
+        }
+        
+        /// <summary>
+        /// Process mouse down events on the room node graph (not over a node)
+        /// </summary>
+        private void ProcessMouseDownEvent(Event currentEvent)
+        {
+            switch (currentEvent.button)
+            {
+                case 0:
+                    ClearLineDrag();
+                    ClearAllSelectedRoomNodes();
+                    break;
+                case 1:
+                    ShowContextMenu(currentEvent.mousePosition);
+                    break;
+            }
+        }
+        private void ProcessMouseUpEvent(Event currentEvent)
+        {
+            // If releasing the right mouse button
+            // and currently dragging a line from a room node
+            if (currentEvent.button != 1 || !_currentRoomNodeGraph.roomNodeToDrawLineFrom) return;
+            
+            RoomNodeSO roomNode = GetHoveredRoomNode(currentEvent);
+                
+            if (roomNode)
+            {
+                // add the child room node to the parent room node
+                if (_currentRoomNodeGraph.roomNodeToDrawLineFrom.AddChildRoomNodeIDToRoomNode(roomNode.id))
+                {
+                    // if successful, add the parent room node to the child room node
+                    roomNode.AddParentRoomNodeIDToRoomNode(_currentRoomNodeGraph.roomNodeToDrawLineFrom.id);
+                }
+            }
+            ClearLineDrag();
+        }
+        
+        private void ProcessMouseDragEvent(Event currentEvent)
+        {
+            if (currentEvent.button == 1)
+            {
+                ProcessRightMouseDragEvent(currentEvent);
+            }
+        }
+        
+        private void ProcessRightMouseDragEvent(Event currentEvent)
+        {
+            if (_currentRoomNodeGraph.roomNodeToDrawLineFrom)
+            {
+                DragConnectingLine(currentEvent.delta);
+                GUI.changed = true;
+            }
+        }
+
+        private void ShowContextMenu(Vector2 mousePosition)
+        {
+            GenericMenu contextMenu = new GenericMenu();
+            contextMenu.AddItem(new GUIContent("Add Room Node"), false, () => AddRoomNode(mousePosition));
+            contextMenu.ShowAsContext();
+        }
+        #endregion Event Processors
+        
+        #region Room Node
+        /// <summary>
+        /// Add a new room node at the mouse position with the default room node type
+        /// </summary>
+        private void AddRoomNode(object mousePositionObject)
+        {
+            // If there are no room nodes, add an entrance node first
+            if (_currentRoomNodeGraph.roomNodeList.Count == 0)
+            {
+                AddRoomNode(new Vector2(200f, 200f),_roomNodeTypeList.list.Find(x => x.isEntrance));
+            }
+            
+            AddRoomNode(mousePositionObject,_roomNodeTypeList.list.Find(x => x.isNone));
+        }
+        
+        /// <summary>
+        /// Add a new room node at the mouse position and with the specified room node type
+        /// </summary>
+        private void AddRoomNode(object mousePositionObject, RoomNodeTypeSO roomNodeType)
+        {
+            Vector2 mousePosition = (Vector2) mousePositionObject;
+            
+            RoomNodeSO roomNode = CreateInstance<RoomNodeSO>();
+            
+            _currentRoomNodeGraph.roomNodeList.Add(roomNode);
+            
+            roomNode.Initialize(new Rect(mousePosition, new Vector2(NodeWidth, NodeHeight)), _currentRoomNodeGraph, roomNodeType);
+            
+            AssetDatabase.AddObjectToAsset(roomNode, _currentRoomNodeGraph);
+            AssetDatabase.SaveAssets();
+            
+            // Repopulate the dictionary
+            _currentRoomNodeGraph.OnValidate();
+        }
+        
+        private void DrawRoomNodes()
+        {
+            foreach (var roomNode in _currentRoomNodeGraph.roomNodeList)
+            {
+                roomNode.Draw(roomNode.isSelected ? _roomNodeSelectedStyle : _roomNodeStyle);
+            }
+
+            GUI.changed = true;
+        }
+        
+        /// <summary>
+        /// Return the room node that is currently being hovered over by the mouse
+        /// </summary>
+        private RoomNodeSO GetHoveredRoomNode(Event currentEvent)
+        {
+            for (int i = _currentRoomNodeGraph.roomNodeList.Count - 1; i >= 0; i--)
+            {
+                if (_currentRoomNodeGraph.roomNodeList[i].rect.Contains(currentEvent.mousePosition))
+                {
+                    return _currentRoomNodeGraph.roomNodeList[i];
+                }
+            }
+            return null;
+        }
+
+        private void ClearAllSelectedRoomNodes()
+        {
+            foreach (RoomNodeSO roomNode in _currentRoomNodeGraph.roomNodeList)
+            {
+                if (roomNode.isSelected)
+                {
+                    roomNode.isSelected = false;
+                    GUI.changed = true;
+                }
+            }
+        }
+        #endregion Room Node
+        
+        #region Room Connection Line
         private void DrawRoomConnections()
         {
             // loop through all room nodes
@@ -187,194 +372,24 @@ namespace NodeGraph.Editor
             }
         }
 
-
-        private void ProcessEvents(Event currentEvent)
-        {
-            
-            
-            // If the node is null
-            // or if the mouse is not currently dragging the node 
-            // then set the current room node
-            if (!_currentRoomNode || _currentRoomNode.isLeftClickDragging == false)
-            {
-                _currentRoomNode = GetHoveredRoomNode(currentEvent);
-            }
-            
-            // If the current room node is null,
-            // or we are currently dragging a line from the room node
-            // then process the room node graph events
-            if (!_currentRoomNode || _currentRoomNodeGraph.roomNodeToDrawLineFrom)
-            {
-                ProcessRoomNodeGraphEvents(currentEvent);
-            }
-            // Else process the room node events
-            else
-            {
-                _currentRoomNode.ProcessEvents(currentEvent);
-            }
-        }
-
         /// <summary>
-        /// Return the room node that is currently being hovered over by the mouse
+        /// Clear connection line
         /// </summary>
-        private RoomNodeSO GetHoveredRoomNode(Event currentEvent)
-        {
-            for (int i = _currentRoomNodeGraph.roomNodeList.Count - 1; i >= 0; i--)
-            {
-                if (_currentRoomNodeGraph.roomNodeList[i].rect.Contains(currentEvent.mousePosition))
-                {
-                    return _currentRoomNodeGraph.roomNodeList[i];
-                }
-            }
-            return null;
-        }
-
-        private void ProcessRoomNodeGraphEvents(Event currentEvent)
-        {
-            switch (currentEvent.type)
-            {
-                case EventType.MouseDown:
-                    ProcessMouseDownEvent(currentEvent);
-                    break;
-                
-                case EventType.MouseUp:
-                    ProcessMouseUpEvent(currentEvent);
-                    break;
-                
-                case EventType.MouseDrag:
-                    ProcessMouseDragEvent(currentEvent);
-                    break;
-                
-            }
-        }
-
-        private void ProcessMouseUpEvent(Event currentEvent)
-        {
-            // If releasing the right mouse button
-            // and currently dragging a line from a room node
-            if (currentEvent.button != 1 || !_currentRoomNodeGraph.roomNodeToDrawLineFrom) return;
-            
-            RoomNodeSO roomNode = GetHoveredRoomNode(currentEvent);
-                
-            if (roomNode)
-            {
-                // add the child room node to the parent room node
-                if (_currentRoomNodeGraph.roomNodeToDrawLineFrom.AddChildRoomNodeIDToRoomNode(roomNode.id))
-                {
-                    // if successful, add the parent room node to the child room node
-                    roomNode.AddParentRoomNodeIDToRoomNode(_currentRoomNodeGraph.roomNodeToDrawLineFrom.id);
-                }
-            }
-            ClearLineDrag();
-        }
-
         private void ClearLineDrag()
         {
             _currentRoomNodeGraph.roomNodeToDrawLineFrom = null;
             _currentRoomNodeGraph.linePosition = Vector2.zero;
             GUI.changed = true;
         }
-
-        private void ProcessMouseDragEvent(Event currentEvent)
-        {
-            if (currentEvent.button == 1)
-            {
-                ProcessRightMouseDragEvent(currentEvent);
-            }
-        }
         
-        private void ProcessRightMouseDragEvent(Event currentEvent)
-        {
-            if (_currentRoomNodeGraph.roomNodeToDrawLineFrom)
-            {
-                DragConnectingLine(currentEvent.delta);
-                GUI.changed = true;
-            }
-        }
-
         private void DragConnectingLine(Vector2 currentEventDelta)
         {
             _currentRoomNodeGraph.linePosition += currentEventDelta;
         }
+        #endregion Room Connection Line
 
-        /// <summary>
-        /// Process mouse down events on the room node graph (not over a node)
-        /// </summary>
-        private void ProcessMouseDownEvent(Event currentEvent)
-        {
-            switch (currentEvent.button)
-            {
-                case 0:
-                    ClearLineDrag();
-                    ClearAllSelectedRoomNodes();
-                    break;
-                case 1:
-                    ShowContextMenu(currentEvent.mousePosition);
-                    break;
-            }
-        }
-
-        private void ClearAllSelectedRoomNodes()
-        {
-            foreach (RoomNodeSO roomNode in _currentRoomNodeGraph.roomNodeList)
-            {
-                if (roomNode.isSelected)
-                {
-                    roomNode.isSelected = false;
-                    GUI.changed = true;
-                }
-            }
-        }
-
-        private void ShowContextMenu(Vector2 mousePosition)
-        {
-            GenericMenu contextMenu = new GenericMenu();
-            contextMenu.AddItem(new GUIContent("Add Room Node"), false, () => AddRoomNode(mousePosition));
-            contextMenu.ShowAsContext();
-        }
-
-        /// <summary>
-        /// Add a new room node at the mouse position with the default room node type
-        /// </summary>
-        private void AddRoomNode(object mousePositionObject)
-        {
-            // If there are no room nodes, add an entrance node first
-            if (_currentRoomNodeGraph.roomNodeList.Count == 0)
-            {
-                AddRoomNode(new Vector2(200f, 200f),_roomNodeTypeList.list.Find(x => x.isEntrance));
-            }
-            
-            AddRoomNode(mousePositionObject,_roomNodeTypeList.list.Find(x => x.isNone));
-        }
         
-        /// <summary>
-        /// Add a new room node at the mouse position and with the specified room node type
-        /// </summary>
-        private void AddRoomNode(object mousePositionObject, RoomNodeTypeSO roomNodeType)
-        {
-            Vector2 mousePosition = (Vector2) mousePositionObject;
-            
-            RoomNodeSO roomNode = CreateInstance<RoomNodeSO>();
-            
-            _currentRoomNodeGraph.roomNodeList.Add(roomNode);
-            
-            roomNode.Initialize(new Rect(mousePosition, new Vector2(NodeWidth, NodeHeight)), _currentRoomNodeGraph, roomNodeType);
-            
-            AssetDatabase.AddObjectToAsset(roomNode, _currentRoomNodeGraph);
-            AssetDatabase.SaveAssets();
-            
-            // Repopulate the dictionary
-            _currentRoomNodeGraph.OnValidate();
-        }
         
-        private void DrawRoomNodes()
-        {
-            foreach (var roomNode in _currentRoomNodeGraph.roomNodeList)
-            {
-                roomNode.Draw(roomNode.isSelected ? _roomNodeSelectedStyle : _roomNodeStyle);
-            }
-
-            GUI.changed = true;
-        }
+        
     }
 }
