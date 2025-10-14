a194f865941c8df9ab3615c9c2292729f78d4cf2
nihanbir <nihan.bir@gmail.com>
2025-10-15 00:16:13 +0200

    create readonly attribute and drawer

---
+++ Assets/Scripts/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,23 @@
+using Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            bool wasEnabled = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = wasEnabled;
+        }
+    
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+    }
+}
